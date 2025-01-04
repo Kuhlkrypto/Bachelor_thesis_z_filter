7a3f26239b25e9bf3d3ccaae27e08aba863e1fa4
Fabian Sandkuhl <fabian.sandkuhl@gmail.com>
2025-01-04 19:50:18 +0100

    Tests um zu überprüfen ob auch wirklich genau auf die Grenze z-1 der user geachtet wird

--- src/z_filter/lru_manager.rs
+++ src/z_filter/lru_manager.rs
@@ -32,12 +32,12 @@ impl LruManager {
 
     fn process(&mut self, case: String, activity: String, timestamp: DateTime<Utc>, source: Vec<String>) -> Option<EventSource> {
         let lru = self.cache.entry(Attribute::new(&activity)).or_insert(VecDeque::new());
-        Self::check_attribute(lru, &case, &timestamp);
+        Self::check_user(lru, &case, &timestamp);
         Self::evict_old_users(lru, &timestamp, &self.max_age);
         Self::exceed_z(lru, self.max_users, case, activity, source, timestamp)
     }
 
-    fn check_attribute(lru: &mut VecDeque<LRUEntry>, user: &String, timestamp: &DateTime<Utc>) {
+    fn check_user(lru: &mut VecDeque<LRUEntry>, user: &String, timestamp: &DateTime<Utc>) {
         //check if user already exists for the attribute
         if let Some(pos) = lru.iter().position(|entry| { entry.user == *user }) {
             //user exists at position 'pos' in the lru
@@ -68,11 +68,105 @@ impl LruManager {
                 activity: String,
                 source: Vec<String>,
                 timestamp: DateTime<Utc>) -> Option<EventSource> {
-        //sanity
+        // Publish Event Source if Lru entry is long enough (length greater equal Z/max_users)
         if lru.len() >= max_users {
             Some(EventSource::new(case, Some(activity), source, Some(timestamp)))
         } else {
             None
         }
+    }
+}
+
+mod tests{
+    use super::*;
+
+    static  Z: usize = 2;
+    fn init_event_source(a: String, u: String) -> EventSource{
+        EventSource::new(
+            u,
+            Some(a.clone()),
+            vec![a],
+            Some(Utc::now()))
+    }
+    fn init_event_sources_equal_user_activity(i: u32) -> Vec<EventSource> {
+        let mut vec = Vec::new();
+        for j in 0..i{
+            vec.push(init_event_source(j.to_string(), j.to_string()));
+
+        }
+
+        vec
+
+    }
+    #[test]
+    fn test_distinct_attributes_distinct_user(){
+        let  res = init_event_sources_equal_user_activity(10);
+        let mut lru = LruManager::from(Config::new(Z, Duration::hours(10)));
+
+        for e in res{
+            assert!(lru.process_event(e).is_none());
+        }
+    }
+
+    #[test]
+    fn test_distinct_attributes_same_user(){
+        let mut vec = Vec::new();
+        let user = String::from("test");
+        for i in 0..10{
+            vec.push(init_event_source(i.to_string(), user.clone()));
+        }
+        let mut lru = LruManager::from(Config::new(Z, Duration::hours(10)));
+        for e in vec{
+            assert!(lru.process_event(e).is_none());
+        }
+    }
+
+    #[test]
+    fn test_different_users_same_attribute(){
+        let attribute: String = String::from("test");
+        let mut vec = Vec::new();
+        for i in 0..10{
+            vec.push(init_event_source(attribute.clone(), i.to_string()));
+        }
+        let mut lru = LruManager::from(Config::new(Z, Duration::hours(10)));
+        for (i,e) in vec.into_iter().enumerate(){
+            let res = lru.process_event(e);
+            println!("{:?}", res);
+            if i < Z -1{
+            assert!(res.is_none());
+            } else {
+                assert!(res.is_some());
+            }
+        }
+    }
+
+    #[test]
+    /// A test whether the eviction loop really evicts user before looking whether it should be outputted
+    fn test_delta_time_limit(){
+        let mut vec = Vec::new();
+        let user = String::from("test");
+        vec.push(EventSource::new(user.clone(), Some(user.clone()), vec![], Some(Utc::now())));
+        vec.push(EventSource::new(String::from("lol"), Some(user.clone()), vec![], Some(Utc::now() + Duration::hours(10))));
+        let mut lru = LruManager::from(Config::new(Z, Duration::hours(10)));
+        for e in vec.into_iter(){
+            assert!(lru.process_event(e).is_none());
+        }
+
+    }
+
+    #[test]
+    fn test_delta_time(){
+        let mut vec = Vec::new();
+        let user = String::from("test");
+        vec.push(EventSource::new(user.clone(), Some(user.clone()), vec![], Some(Utc::now())));
+        vec.push(EventSource::new(String::from("lol"), Some(user.clone()), vec![], Some(Utc::now() + Duration::hours(9))));
+        let mut lru = LruManager::from(Config::new(Z, Duration::hours(10)));
+        for (i,e) in vec.into_iter().enumerate(){
+            let res = lru.process_event(e);
+            if i == 0{
+                assert!(res.is_none());
+            } else {
+                assert!(res.is_some());
+            }        }
     }
 }

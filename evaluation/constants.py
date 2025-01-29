8cfaffa2c7c3df710770ab20c787b555d78c0fab
Fabian Sandkuhl <fabian.sandkuhl@gmail.com>
2025-01-29 13:46:09 +0100

    next evaluation BPI 2012

--- evaluation/constants.py
+++ evaluation/constants.py
@@ -7,10 +7,8 @@ COL_NAME_CASE_IDENT = 'case_id'
 COL_NAME_ACTIVITY = 'activity'
 ABSTRACTED_NAME_SUFFIX = 'abstracted'
 PATH_TMP = os.path.join(os.getcwd(), "tmp/")
-# FILTERING_ABSOLUTE_ZS = {2, 3, 4, 5, 15, 30, 45, 60}
-FILTERING_ABSOLUTE_ZS =  set(range(30))
-# FILTERING_RELATIVE_ZS = [0.001, 0.005, 0.01, 0.05, 0.1, 0.15, 0.2]
-FILTERING_RELATIVE_ZS = {}
+FILTERING_ABSOLUTE_ZS = {2, 3, 4, 5, 15, 30, 45, 60}
+FILTERING_RELATIVE_ZS = [0.005, 0.01, 0.05, 0.1, 0.15, 0.2, 0.3]
 # FILTERING_TIME_DELTAS = ['1h', '24h', '72h', 'inf']
 FILTERING_MODES = ['0','1']
 
